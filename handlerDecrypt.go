f7ac78537185ef393e349121260de903e1d38f22
James Rosewell <james@51degrees.com>
2022-10-11 19:52:05 +0100

    UPDATE: Uses the correct SWAN model which will not set members unless they are valid data structures.

--- handlerDecrypt.go
+++ handlerDecrypt.go
@@ -144,7 +144,7 @@ func createResponseModel(
 	// operators where DeleteDays contains the number of days that should elapse
 	// before the data is automatically removed.
 	e := s.config.DeleteDays
-	if m.RID != nil && m.RID.IsSigned() {
+	if m.RID != nil {
 		m.RID.GetCookie().Expires = m.RID.GetOWID().GetExpires(e)
 	}
 	if m.Pref != nil {
